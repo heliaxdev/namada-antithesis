dcb366c1777edd9b9e4902dc2f7dae5bcb0b3caa
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-06 16:26:24 +0100

    added init-account tx to docker

---
+++ workload/scripts/parallel_driver_init_account.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+/app/namada-chain-workload --rpc http://${RPC} --chain-id ${CHAIN_ID} --faucet-sk ${FAUCET_SK} --id ${WORKLOAD_ID} --masp-indexer-url ${MASP_INDEXER_URL} init-account
