fcd5b8a72fa73dab640ff8cf5e5a518e46ec9ec7
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-02-06 16:28:15 +0100

    wip

--- workload/src/build_checks/become_validator.rs
+++ workload/src/build_checks/become_validator.rs
@@ -1,9 +1,5 @@
-use tryhard::{backoff_strategies::ExponentialBackoff, NoOnRetry, RetryFutureConfig};
+use crate::{check::Check, entities::Alias};
 
-use crate::{check::Check, entities::Alias, sdk::namada::Sdk, state::State};
-
-pub async fn become_validator(
-    source: Alias,
-) -> Vec<Check> {
+pub async fn become_validator(source: Alias) -> Vec<Check> {
     vec![Check::IsValidatorAccount(source)]
 }
