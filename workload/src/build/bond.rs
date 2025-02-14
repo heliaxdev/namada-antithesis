f83d29527590b92c17749285eedf99400071df1c
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-02-14 11:30:50 +0100

    remove account balance once its a validator

--- workload/src/build/bond.rs
+++ workload/src/build/bond.rs
@@ -2,7 +2,12 @@ use namada_sdk::rpc;
 use rand::seq::IteratorRandom;
 
 use crate::{
-    constants::MIN_TRANSFER_BALANCE, entities::Alias, sdk::namada::Sdk, state::State, steps::StepError, task::{Task, TaskSettings}
+    constants::MIN_TRANSFER_BALANCE,
+    entities::Alias,
+    sdk::namada::Sdk,
+    state::State,
+    steps::StepError,
+    task::{Task, TaskSettings},
 };
 
 use super::utils;
@@ -10,7 +15,7 @@ use super::utils;
 pub async fn build_bond(sdk: &Sdk, state: &mut State) -> Result<Vec<Task>, StepError> {
     let client = sdk.namada.clone_client();
     let source_account = state
-        .random_account_with_min_balance(vec![],  MIN_TRANSFER_BALANCE)
+        .random_account_with_min_balance(vec![], MIN_TRANSFER_BALANCE)
         .ok_or(StepError::Build("No more accounts".to_string()))?;
     let amount_account = state.get_balance_for(&source_account.alias);
     let amount = utils::random_between(state, 1, amount_account);
