heliaxdev/namada-resilience-test

replace sdk with context

workload/src/utils/tx.rs
@@ -16,8 +16,8 @@ use namada_sdk::tx::{
 use namada_sdk::Namada;
 
 use crate::constants::DEFAULT_GAS_LIMIT;
+use crate::context::Ctx;
 use crate::error::TaskError;
-use crate::sdk::namada::Sdk;
 use crate::task::TaskSettings;
 use crate::types::{Alias, Height};
 
@@ -52,8 +52,8 @@ fn get_tx_errors(
     None
 }
 
-async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
-    let wallet = sdk.namada.wallet.read().await;
+async fn default_tx_arg(ctx: &Ctx) -> args::Tx {
+    let wallet = ctx.namada.wallet.read().await;
     let nam = wallet
         .find_address("nam")
         .expect("Native token should be present.")
@@ -66,7 +66,8 @@ async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
         output_folder: None,
         force: false,
         broadcast_only: false,
-        ledger_address: tendermint_rpc::Url::from_str("http://127.0.0.1:26657").unwrap(),
+        ledger_address: namada_sdk::tendermint_rpc::Url::from_str("http://127.0.0.1:26657")
+            .unwrap(),
         initialized_account_alias: None,
         wallet_alias_force: false,
         fee_amount: None,
@@ -88,16 +89,16 @@ async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
 }
 
 pub async fn build_reveal_pk(
-    sdk: &Sdk,
+    ctx: &Ctx,
     public_key: common::PublicKey,
 ) -> Result<(Tx, Vec<SigningTxData>, args::Tx), TaskError> {
-    let wallet = sdk.namada.wallet.read().await;
+    let wallet = ctx.namada.wallet.read().await;
     let fee_payer = wallet
         .find_public_key(Alias::faucet().name)
         .map_err(|e| TaskError::Wallet(e.to_string()))?;
     drop(wallet);
 
-    let reveal_pk_tx_builder = sdk
+    let reveal_pk_tx_builder = ctx
         .namada
         .new_reveal_pk(public_key.clone())
         .signing_keys(vec![public_key])
@@ -105,7 +106,7 @@ pub async fn build_reveal_pk(
         .wrapper_fee_payer(fee_payer);
 
     let (reveal_tx, signing_data) = reveal_pk_tx_builder
-        .build(&sdk.namada)
+        .build(&ctx.namada)
         .await
         .map_err(|e| TaskError::BuildTx(e.to_string()))?;
 
@@ -113,25 +114,25 @@ pub async fn build_reveal_pk(
 }
 
 pub async fn execute_reveal_pk(
-    sdk: &Sdk,
+    ctx: &Ctx,
     public_key: common::PublicKey,
 ) -> Result<Height, TaskError> {
-    let (tx, signing_data, tx_args) = build_reveal_pk(sdk, public_key).await?;
+    let (tx, signing_data, tx_args) = build_reveal_pk(ctx, public_key).await?;
 
-    execute_tx(sdk, tx, signing_data, &tx_args).await
+    execute_tx(ctx, tx, signing_data, &tx_args).await
 }
 
 pub async fn merge_tx(
-    sdk: &Sdk,
+    ctx: &Ctx,
     txs: Vec<(Tx, SigningTxData)>,
     settings: &TaskSettings,
 ) -> Result<(Tx, Vec<SigningTxData>, args::Tx), TaskError> {
     if txs.is_empty() {
         return Err(TaskError::BuildTx("Empty tx batch".to_string()));
     }
-    let tx_args = default_tx_arg(sdk).await;
+    let tx_args = default_tx_arg(ctx).await;
 
-    let wallet = sdk.namada.wallet.read().await;
+    let wallet = ctx.namada.wallet.read().await;
     let faucet_alias = Alias::faucet();
     let gas_payer_pk = wallet
         .find_public_key(faucet_alias.name)
@@ -162,7 +163,7 @@ pub async fn merge_tx(
 }
 
 pub(crate) async fn execute_tx(
-    sdk: &Sdk,
+    ctx: &Ctx,
     tx: Tx,
     signing_datas: Vec<SigningTxData>,
     tx_args: &args::Tx,
@@ -170,9 +171,9 @@ pub(crate) async fn execute_tx(
     let mut tx = tx;
 
     let is_batch = tx.commitments().len() > 1;
-    do_sign_tx(sdk, &mut tx, signing_datas, tx_args).await;
+    do_sign_tx(ctx, &mut tx, signing_datas, tx_args).await;
     if is_batch {
-        let gas_payer_sk = sdk
+        let gas_payer_sk = ctx
             .namada
             .wallet_mut()
             .await
@@ -189,10 +190,10 @@ pub(crate) async fn execute_tx(
     let tx_hash = tx.header_hash().to_string();
     let wrapper_hash = tx.wrapper_hash();
 
-    let tx_response = match sdk.namada.submit(tx, tx_args).await {
+    let tx_response = match ctx.namada.submit(tx, tx_args).await {
         Ok(response) => response,
         Err(NamadaError::Tx(TxSubmitError::AppliedTimeout)) => {
-            retry_tx_status_check(sdk, tx_args, &tx_hash, wrapper_hash, &cmts).await?
+            retry_tx_status_check(ctx, tx_args, &tx_hash, wrapper_hash, &cmts).await?
         }
         Err(e) => return Err(TaskError::Broadcast(e)),
     };
@@ -231,9 +232,9 @@ pub(crate) async fn execute_tx(
     Ok(height)
 }
 
-async fn do_sign_tx(sdk: &Sdk, tx: &mut Tx, signing_datas: Vec<SigningTxData>, tx_args: &args::Tx) {
+async fn do_sign_tx(ctx: &Ctx, tx: &mut Tx, signing_datas: Vec<SigningTxData>, tx_args: &args::Tx) {
     for signing_data in signing_datas {
-        sdk.namada
+        ctx.namada
             .sign(tx, tx_args, signing_data, default_sign, ())
             .await
             .expect("unable to sign tx");
@@ -241,7 +242,7 @@ async fn do_sign_tx(sdk: &Sdk, tx: &mut Tx, signing_datas: Vec<SigningTxData>, t
 }
 
 async fn retry_tx_status_check(
-    sdk: &Sdk,
+    ctx: &Ctx,
     tx_args: &args::Tx,
     tx_hash: &str,
     wrapper_hash: Option<Hash>,
@@ -251,7 +252,7 @@ async fn retry_tx_status_check(
 
     let tx_query = rpc::TxEventQuery::Applied(tx_hash);
     let deadline = time::Instant::now() + time::Duration::from_secs(300);
-    let events = rpc::query_tx_status(&sdk.namada, tx_query, deadline)
+    let events = rpc::query_tx_status(&ctx.namada, tx_query, deadline)
         .await
         .map_err(TaskError::Broadcast)?;
     let tx_response = TxResponse::from_events(events);
@@ -261,7 +262,7 @@ async fn retry_tx_status_check(
         if let Some(InnerTxResult::Success(result)) = tx_response.batch_result().get(
             &compute_inner_tx_hash(wrapper_hash.as_ref(), either::Right(cmt)),
         ) {
-            save_initialized_accounts(&sdk.namada, tx_args, result.initialized_accounts.clone())
+            save_initialized_accounts(&ctx.namada, tx_args, result.initialized_accounts.clone())
                 .await;
         }
     }

heliaxdev/namada-antithesis

workload/src/utils/tx.rs
@@ -16,8 +16,8 @@ use namada_sdk::tx::{
 use namada_sdk::Namada;
 
 use crate::constants::DEFAULT_GAS_LIMIT;
+use crate::context::Ctx;
 use crate::error::TaskError;
-use crate::sdk::namada::Sdk;
 use crate::task::TaskSettings;
 use crate::types::{Alias, Height};
 
@@ -52,8 +52,8 @@ fn get_tx_errors(
     None
 }
 
-async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
-    let wallet = sdk.namada.wallet.read().await;
+async fn default_tx_arg(ctx: &Ctx) -> args::Tx {
+    let wallet = ctx.namada.wallet.read().await;
     let nam = wallet
         .find_address("nam")
         .expect("Native token should be present.")
@@ -66,7 +66,8 @@ async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
         output_folder: None,
         force: false,
         broadcast_only: false,
-        ledger_address: tendermint_rpc::Url::from_str("http://127.0.0.1:26657").unwrap(),
+        ledger_address: namada_sdk::tendermint_rpc::Url::from_str("http://127.0.0.1:26657")
+            .unwrap(),
         initialized_account_alias: None,
         wallet_alias_force: false,
         fee_amount: None,
@@ -88,16 +89,16 @@ async fn default_tx_arg(sdk: &Sdk) -> args::Tx {
 }
 
 pub async fn build_reveal_pk(
-    sdk: &Sdk,
+    ctx: &Ctx,
     public_key: common::PublicKey,
 ) -> Result<(Tx, Vec<SigningTxData>, args::Tx), TaskError> {
-    let wallet = sdk.namada.wallet.read().await;
+    let wallet = ctx.namada.wallet.read().await;
     let fee_payer = wallet
         .find_public_key(Alias::faucet().name)
         .map_err(|e| TaskError::Wallet(e.to_string()))?;
     drop(wallet);
 
-    let reveal_pk_tx_builder = sdk
+    let reveal_pk_tx_builder = ctx
         .namada
         .new_reveal_pk(public_key.clone())
         .signing_keys(vec![public_key])
@@ -105,7 +106,7 @@ pub async fn build_reveal_pk(
         .wrapper_fee_payer(fee_payer);
 
     let (reveal_tx, signing_data) = reveal_pk_tx_builder
-        .build(&sdk.namada)
+        .build(&ctx.namada)
         .await
         .map_err(|e| TaskError::BuildTx(e.to_string()))?;
 
@@ -113,25 +114,25 @@ pub async fn build_reveal_pk(
 }
 
 pub async fn execute_reveal_pk(
-    sdk: &Sdk,
+    ctx: &Ctx,
     public_key: common::PublicKey,
 ) -> Result<Height, TaskError> {
-    let (tx, signing_data, tx_args) = build_reveal_pk(sdk, public_key).await?;
+    let (tx, signing_data, tx_args) = build_reveal_pk(ctx, public_key).await?;
 
-    execute_tx(sdk, tx, signing_data, &tx_args).await
+    execute_tx(ctx, tx, signing_data, &tx_args).await
 }
 
 pub async fn merge_tx(
-    sdk: &Sdk,
+    ctx: &Ctx,
     txs: Vec<(Tx, SigningTxData)>,
     settings: &TaskSettings,
 ) -> Result<(Tx, Vec<SigningTxData>, args::Tx), TaskError> {
     if txs.is_empty() {
         return Err(TaskError::BuildTx("Empty tx batch".to_string()));
     }
-    let tx_args = default_tx_arg(sdk).await;
+    let tx_args = default_tx_arg(ctx).await;
 
-    let wallet = sdk.namada.wallet.read().await;
+    let wallet = ctx.namada.wallet.read().await;
     let faucet_alias = Alias::faucet();
     let gas_payer_pk = wallet
         .find_public_key(faucet_alias.name)
@@ -162,7 +163,7 @@ pub async fn merge_tx(
 }
 
 pub(crate) async fn execute_tx(
-    sdk: &Sdk,
+    ctx: &Ctx,
     tx: Tx,
     signing_datas: Vec<SigningTxData>,
     tx_args: &args::Tx,
@@ -170,9 +171,9 @@ pub(crate) async fn execute_tx(
     let mut tx = tx;
 
     let is_batch = tx.commitments().len() > 1;
-    do_sign_tx(sdk, &mut tx, signing_datas, tx_args).await;
+    do_sign_tx(ctx, &mut tx, signing_datas, tx_args).await;
     if is_batch {
-        let gas_payer_sk = sdk
+        let gas_payer_sk = ctx
             .namada
             .wallet_mut()
             .await
@@ -189,10 +190,10 @@ pub(crate) async fn execute_tx(
     let tx_hash = tx.header_hash().to_string();
     let wrapper_hash = tx.wrapper_hash();
 
-    let tx_response = match sdk.namada.submit(tx, tx_args).await {
+    let tx_response = match ctx.namada.submit(tx, tx_args).await {
         Ok(response) => response,
         Err(NamadaError::Tx(TxSubmitError::AppliedTimeout)) => {
-            retry_tx_status_check(sdk, tx_args, &tx_hash, wrapper_hash, &cmts).await?
+            retry_tx_status_check(ctx, tx_args, &tx_hash, wrapper_hash, &cmts).await?
         }
         Err(e) => return Err(TaskError::Broadcast(e)),
     };
@@ -231,9 +232,9 @@ pub(crate) async fn execute_tx(
     Ok(height)
 }
 
-async fn do_sign_tx(sdk: &Sdk, tx: &mut Tx, signing_datas: Vec<SigningTxData>, tx_args: &args::Tx) {
+async fn do_sign_tx(ctx: &Ctx, tx: &mut Tx, signing_datas: Vec<SigningTxData>, tx_args: &args::Tx) {
     for signing_data in signing_datas {
-        sdk.namada
+        ctx.namada
             .sign(tx, tx_args, signing_data, default_sign, ())
             .await
             .expect("unable to sign tx");
@@ -241,7 +242,7 @@ async fn do_sign_tx(sdk: &Sdk, tx: &mut Tx, signing_datas: Vec<SigningTxData>, t
 }
 
 async fn retry_tx_status_check(
-    sdk: &Sdk,
+    ctx: &Ctx,
     tx_args: &args::Tx,
     tx_hash: &str,
     wrapper_hash: Option<Hash>,
@@ -251,7 +252,7 @@ async fn retry_tx_status_check(
 
     let tx_query = rpc::TxEventQuery::Applied(tx_hash);
     let deadline = time::Instant::now() + time::Duration::from_secs(300);
-    let event = rpc::query_tx_status(&sdk.namada, tx_query, deadline)
+    let event = rpc::query_tx_status(&ctx.namada, tx_query, deadline)
         .await
         .map_err(TaskError::Broadcast)?;
     let tx_response = TxResponse::from_event(event);
@@ -261,7 +262,7 @@ async fn retry_tx_status_check(
         if let Some(InnerTxResult::Success(result)) = tx_response.batch_result().get(
             &compute_inner_tx_hash(wrapper_hash.as_ref(), either::Right(cmt)),
         ) {
-            save_initialized_accounts(&sdk.namada, tx_args, result.initialized_accounts.clone())
+            save_initialized_accounts(&ctx.namada, tx_args, result.initialized_accounts.clone())
                 .await;
         }
     }